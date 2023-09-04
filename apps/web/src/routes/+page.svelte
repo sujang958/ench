3879b46e293b9a3a10cdfffc74cbfef06df48e1f
sujang.958 <enter7377@naver.com>
2023-09-04 23:37:57 +0900

    feat: add more weights for the font

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<section class="h-screen flex flex-col items-center justify-center">
-	<p class="text-5xl font-bold">HEllo</p>
+<section class="h-screen flex flex-col items-center justify-center text-center gap-y-8">
+	<p class="text-8xl font-bold leading-tight">I can make you <br> read mails</p>
+	<p class="text-2xl text-neutral-600 break-words max-w-3xl w-full font-medium">If you have trouble reading your mail frequently, this app is right for you.
+Ench provides you a chat app-like experience in mail.</p>
 </section>
