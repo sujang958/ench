50813914b7e469429054ed63dddbc3a0bbe79067
sujang.958 <enter7377@naver.com>
2023-09-05 21:37:08 +0900

    feat: add a landing page section

---
+++ apps/web/src/lib/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+</script>
+
+<button
+	type="button"
+	class={twMerge('rounded-xl text-center px-4 py-[0.29rem] text-lg font-medium text-white bg-black', $$restProps.class ?? '')}
+>
+	<slot />
+</button>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,5 +1,16 @@
-<section class="h-screen flex flex-col items-center justify-center text-center gap-y-8">
-	<p class="text-8xl font-bold leading-tight">I can make you <br> read mails</p>
-	<p class="text-2xl text-neutral-600 break-words max-w-3xl w-full font-medium">If you have trouble reading your mail frequently, this app is right for you.
-Ench provides you a chat app-like experience in mail.</p>
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<section class="h-screen flex flex-col items-center justify-center text-center gap-y-12">
+	<p class="text-8xl font-bold leading-tight">I can make you <br /> read mails</p>
+	<p class="text-2xl text-neutral-600 break-words max-w-3xl w-full font-medium">
+		If you have trouble reading your mail frequently, this app is right for you. Ench provides you a
+		chat app-like experience in mail.
+	</p>
+	<div class="py-1" />
+	<div class="flex flex-row items-center gap-x-4">
+		<Button class="text-black bg-neutral-200 font-bold">About</Button>
+		<Button>Sign Up</Button>
+	</div>
 </section>
